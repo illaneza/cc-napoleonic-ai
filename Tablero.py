2c50e397cc148d6cf384bda6c4adf6cf37b4d332
illaneza <illaneza@hotmail.com>
2025-08-10 14:23:31 +0200

    Update Tablero.py

--- Tablero.py
+++ Tablero.py
@@ -1,1 +1,18 @@
-
+def mostrar_tablero(unidades, terreno):
+    print("Tablero 15x15:")
+    for fila in range(15):
+        linea = ""
+        for col in range(15):
+            celda_str = f"{fila}-{col}"
+            ocupado = False
+            for id, u in unidades.items():
+                if u["posicion"] == [fila, col]:
+                    linea += id[0].upper() + "  "
+                    ocupado = True
+                    break
+            if not ocupado:
+                if celda_str in terreno.get("impasables", []):
+                    linea += "#  "
+                else:
+                    linea += ".  "
+        print(linea)
