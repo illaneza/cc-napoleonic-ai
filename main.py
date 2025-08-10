a6530ebe27ccc1ccb4b1da347e7c6c641ade8e80
illaneza <illaneza@hotmail.com>
2025-08-10 15:16:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
-print("Estado inicial del tablero:")
-mostrar_tablero(unidades, terreno)
-ia.ejecutar_turno("Centro")  # Aquí indica el sector ordenado
-print("Estado del tablero después del turno IA:")
-mostrar_tablero(unidades, terreno)
+from ia import IA
+from tablero import mostrar_tablero
+
+unidades = {
+    "U1": {"tipo": "infanteria", "posicion": [7,7], "estado": "completo", "sector": "Centro"},
+    "U2": {"tipo": "caballeria", "posicion": [6,7], "estado": "debilitado", "sector": "Centro"}
+}
+
+terreno = {
+    "impasables": ["0-0", "0-1"]
+}
+
+def main():
+    ia = IA(unidades, terreno)
+    print("Estado inicial del tablero:")
+    mostrar_tablero(unidades, terreno)
+    ia.ejecutar_turno("Centro")
+    print("Estado del tablero después del turno IA:")
+    mostrar_tablero(unidades, terreno)
+
+if __name__ == "__main__":
+    main()
