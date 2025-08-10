0cb2fcf0942702561cfe58fb20167cfa29d8ad9d
illaneza <illaneza@hotmail.com>
2025-08-10 17:53:29 +0200

    Create unidades.py

---
+++ unidades.py
@@ -0,0 +1,7 @@
+# unidades.py
+unidades = {
+    "U1": {"tipo": "I", "pos": (2, 3), "estado": "completo", "equipo": "IA"},
+    "U2": {"tipo": "C", "pos": (4, 7), "estado": "completo", "equipo": "IA"},
+    "U3": {"tipo": "A", "pos": (10, 10), "estado": "debilitado", "equipo": "Jugador"},
+    "U4": {"tipo": "I", "pos": (6, 12), "estado": "completo", "equipo": "Jugador"},
+}
